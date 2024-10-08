b7a716f380ddc92ebe00b17f036f076a09a279e9
andycruz05 <149445786+andycruz05@users.noreply.github.com>
2024-10-07 23:24:58 -0400

    Add files via upload

---
+++ Recursion Lab/ArraySum.java
@@ -0,0 +1,19 @@
+
+public class ArraySum 
+{
+
+		public int sumOfArray (Integer[] a, int index)
+		
+		{
+			int sum;
+			
+			if (index == 0)
+			{
+				sum = a[index];	
+			} else {
+				sum = a[index] + sumOfArray(a, --index);
+			}
+		return sum;
+	}
+
+}
